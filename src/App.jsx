a5574914cf111bc0e7177c4db727c0edc98de38b
Jose Luis Vazquez Flores <jluisvazquez.jv@gmail.com>
2023-03-28 12:03:53 -0600

    Setup react project using vite

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import TodosLogic from './components/TodosLogic';
+import './styles/App.css'
+
+function App() {
+
+  return (
+    <div className="app">
+      <div className="header">
+        <h1>Todos</h1>
+        <p>items will persist in the browser local storage</p>
+      </div>
+        <TodosLogic />
+    </div>
+  )
+}
+
+export default App
